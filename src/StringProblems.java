e93e16f8feefe0ea81585c29a7de1ccd3206693e
soladime <soladime@iulb-lc135b-01.ads.iu.edu>
2018-02-06 07:46:19 -0500

    String Problems

---
+++ src/StringProblems.java
@@ -0,0 +1,10 @@
+/* replace spaces in an array with %20 */
+public class StringProblems {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int[][] c = new int[4][4];
+	}
+	
+	// replace spaces with array
+}
